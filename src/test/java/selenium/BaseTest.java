01a3e316adab9a33a7663577cb24c02340653aa5
Eugene <Evgeny.SENYUKOV@raiffeisen.ru>
2023-04-11 16:50:05 +0300

    init

---
+++ src/test/java/selenium/BaseTest.java
@@ -0,0 +1,31 @@
+package selenium;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class BaseTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @BeforeEach
+    public void setUp() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(1));
+    }
+
+    @Test
+    public void firstTest() {
+        driver.get("https://www.selenium.dev/");
+    }
+
+    @AfterEach
+    public void stop() {
+        driver.close();
+    }
+}
