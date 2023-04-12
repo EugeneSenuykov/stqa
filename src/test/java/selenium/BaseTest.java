d880a9077826be3ff55812a0035c212f990eae6b
Eugene <Evgeny.SENYUKOV@raiffeisen.ru>
2023-04-12 11:16:16 +0300

    first test

--- src/test/java/selenium/BaseTest.java
+++ src/test/java/selenium/BaseTest.java
@@ -3,25 +3,27 @@ package selenium;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
 public class BaseTest {
     private WebDriver driver;
-    private WebDriverWait wait;
 
     @BeforeEach
     public void setUp() {
         driver = new ChromeDriver();
-        wait = new WebDriverWait(driver, Duration.ofSeconds(1));
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
     }
 
     @Test
     public void firstTest() {
-        driver.get("https://www.selenium.dev/");
+        driver.get("http://localhost:8080/litecart/admin");
+        driver.findElement(By.name("username")).sendKeys("admin");
+        driver.findElement(By.name("password")).sendKeys("admin");
+        driver.findElement(By.name("login")).click();
     }
 
     @AfterEach
