afde3df9c4db099daa626cd0f764423a1700e36d
Eugene <Evgeny.SENYUKOV@raiffeisen.ru>
2023-04-20 16:29:01 +0300

    (Доработка) Задание 7. Сделайте сценарий, проверяющий наличие стикеров у товаров

--- src/main/java/selenium/page/customer/MainPage.java
+++ src/main/java/selenium/page/customer/MainPage.java
@@ -1,5 +1,6 @@
 package selenium.page.customer;
 
+import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,11 +8,11 @@ import org.openqa.selenium.WebElement;
 import java.util.List;
 
 public class MainPage {
-    private final By productsList = By.cssSelector("li[class^='product']");
-    private final By sticker = By.cssSelector("div[class^='sticker']");
+    private final By productsList = By.cssSelector("li.product");
+    private final By sticker = By.cssSelector("div.sticker");
 
     public void checkSticker(WebDriver driver) {
         List<WebElement> products = driver.findElements(productsList);
-        products.forEach(e -> e.findElement(sticker).isDisplayed());
+        products.forEach(e -> Assertions.assertEquals(1, e.findElements(sticker).size()));
     }
 }
